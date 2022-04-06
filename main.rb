ce939a2fa1f0359902fef81daccc0d7dcab39fed
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-04-06 17:02:49 -0300

    Created board and added display method

--- main.rb
+++ main.rb
@@ -32,3 +32,17 @@ def player_names
   end
   [player1, player2]
 end
+
+board = Array.new(9)
+num = 1
+board = board.each_index do |ind|
+  board[ind] = num
+  num += 1
+end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
